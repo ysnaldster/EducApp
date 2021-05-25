eba0b7ccb3959d63e08b7e6541a675e0ad36e998
Ysnaldo Lopez <ysnaldolopez@gmail.com>
2021-05-24 22:40:50 -0500

    primer push en la rama ysnaldo para probar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from './components/Home.jsx'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home/>
         <h1>EducApp</h1>
       </header>
     </div>
