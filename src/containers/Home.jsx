23e5e2f4a2b94bed2dded976ebdb44f4d41e8517
Elver Tobo <elvertobo2017@gmail.com>
2021-05-30 18:09:25 -0500

    Se ajusta la vista de resultados

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Header from '../components/Header.jsx'
-
+import React from "react";
+import Header from "../components/Header.jsx";
 
 const Home = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
